d8902c35de5c250adbd445c86737cba7bc5059c1
Roman Yakovliev <Romayakovlev@hotmail.com>
2021-05-30 21:57:19 +0300

    project created

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,7 @@
+import "./intro.scss";
+
+function Intro() {
+  return <div className="intro"></div>;
+}
+
+export default Intro;
---
+++ src/components/testimonials/Testimonials.jsx
@@ -0,0 +1,7 @@
+import "./testimonials.scss";
+
+function Testimonials() {
+  return <div className="testimonials"></div>;
+}
+
+export default Testimonials;
